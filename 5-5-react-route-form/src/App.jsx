2d60311adf2bdc37459ce82267ca60e61d2cffef
Sadeem-Alghamdi <s202244580@kfupm.edu.sa>
2026-03-02 11:21:12 +0300

    1

--- 5-5-react-route-form/src/App.jsx
+++ 5-5-react-route-form/src/App.jsx
@@ -34,7 +34,10 @@
 // ===========================
 // TODO #1: Use app.jsx.jsx file. Apply React Router
 // ===========================
-//
+import { NavLink, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Registration from "./pages/Registration";
 // Goal: Add routing for three pages: Home, About, Registration
 //
 // 1) Import router primitives from react-router-dom
@@ -148,15 +151,35 @@
 export default function App() {
   return (
     <div className="app">
+
       <nav className="navbar">
         <div className="brand">🧑‍💻 Student Portal</div>
         <div className="links">
           {/*Nav links*/}
+
+            <NavLink to="/" end className="navlink">
+                Home
+            </NavLink>
+
+            <NavLink to="/about" className="navlink">
+                About
+            </NavLink>
+
+            <NavLink to="/registration" className="navlink">
+                Registration
+            </NavLink>
         </div>
       </nav>
 
       <main className="container">
         {/*Routes*/}
+
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/registration" element={<Registration />} />
+              <Route path="*" element={<h2>404 — Not Found</h2>} />
+          </Routes>
       </main>
 
       <footer className="footer">
